54b6670e36f838a8a543a3f8c672f359f630ba45
VikashniM <104898068+VikashniM@users.noreply.github.com>
2022-11-03 20:07:48 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # microsoft package
-sudo wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
