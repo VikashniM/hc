ab7326ece4cf24bf9543f7357e35f174410ab0e3
VikashniM <104898068+VikashniM@users.noreply.github.com>
2022-11-03 20:01:09 +0530

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# microsoft package
+sudo wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+#moby engine
+sudo apt-get update
+sudo apt-get install moby-engine
+
+#iot defender
+sudo apt-get update
+sudo apt-get install aziot-edge defender-iot-micro-agent-edge
+
