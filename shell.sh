f28b47b3e4a59d9b689da33ade783f732294ba66
KrithigaM11 <107237009+KrithigaM11@users.noreply.github.com>
2022-11-03 21:42:28 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # microsoft package
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 #moby engine
 sudo apt-get update
-sudo apt-get install -y moby-engine
+yes | sudo apt-get install moby-engine
 
 #iot defender
 sudo apt-get update
-sudo apt-get install -y aziot-edge defender-iot-micro-agent-edge
+yes | sudo apt-get install aziot-edge defender-iot-micro-agent-edge
 
