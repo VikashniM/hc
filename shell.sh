cb0a209278e600606c710b51fc9048252e93a7c9
VikashniM <104898068+VikashniM@users.noreply.github.com>
2022-11-30 17:14:16 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -66,7 +66,10 @@ export VAULT_URL="https://key-edge-dev-infra-001.vault.azure.net"
 
 #pre-req for python script
 sudo apt-get update
-sudo apt install python3-pip
-pip install azure-keyvault-certificates azure-identity azure-keyvault-secrets
+sudo apt install -y python3-pip
+pip install -y azure-keyvault-certificates azure-identity azure-keyvault-secrets
 
 #download python script
+FILEPATH=""
+python3 download.py
+./download.sh
