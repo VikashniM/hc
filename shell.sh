309a15f6ec2e0e0fdc276c842a9e873ec843544d
VikashniM <104898068+VikashniM@users.noreply.github.com>
2022-11-30 12:52:27 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -13,3 +13,47 @@ sudo apt-get install -y moby-engine
 sudo apt-get update
 sudo apt-get install -y aziot-edge defender-iot-micro-agent-edge
 
+#update config file
+sudo sh -c "cat > /etc/aziot/config.toml" << 'EOF' 
+auto_reprovisioning_mode="AlwaysOnStartup"
+[provisioning]
+source = "dps"
+global_endpoint = "https://global.azure-devices-provisioning.net"
+id_scope = "0ne008356BE"
+# Uncomment to send a custom payload during DPS registration
+# payload = { uri = "PATH_TO_JSON_FILE" }
+[provisioning.attestation]
+method = "x509"
+#registration_id = "Azure IoT Hub Intermediate Cert Test Only"
+registration_id = "device-01"
+#identity_cert = "file:///home/adminuser/backup/azure-iot-test-only.intermediate.cert.pem"
+#identity_pk = "file:///home/adminuser/backup/azure-iot-test-only.intermediate.key.pem"
+identity_cert = "file:///home/adminuser/backup/device-01-IIm-full-chain.cert.pem"
+identity_pk = "file:///home/adminuser/backup/device-01.key.pem"
+[aziot_keys]
+[preloaded_keys]
+[cert_issuance]
+[preloaded_certs]
+[tpm]
+[agent]
+name = "edgeAgent"
+type = "docker"
+imagePullPolicy = "on-create"
+[agent.config]
+image = "mcr.microsoft.com/azureiotedge-agent:1.4"
+[agent.config.createOptions]
+[agent.env]
+[connect]
+workload_uri = "unix:///var/run/iotedge/workload.sock"
+management_uri = "unix:///var/run/iotedge/mgmt.sock"
+[listen]
+workload_uri = "fd://aziot-edged.workload.socket"
+management_uri = "fd://aziot-edged.mgmt.socket"
+min_tls_version = "tls1.0"
+[watchdog]
+max_retries = "infinite"
+[moby_runtime]
+uri = "unix:///var/run/docker.sock"
+network = "azure-iot-edge"
+EOF
+
