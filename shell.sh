c07433c366c310e351dfb989c4046d1a59fca7a0
VikashniM <104898068+VikashniM@users.noreply.github.com>
2022-12-01 16:11:53 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -64,15 +64,15 @@ network = "azure-iot-edge"
 EOF
 
 #set env variables
-#export AZURE_CLIENT_ID="5a50ed53-0a78-4b7b-9b74-c301a663aed6"
-#export AZURE_CLIENT_SECRET="mrD8Q~OkYGaKHpLugvoI0hrWuQiEXj8usDsj7atv"
-#export AZURE_TENANT_ID="6bc44698-b191-4f2e-97ac-cd7b430088cf"
-#export VAULT_URL="https://key-edge-dev-infra-001.vault.azure.net"
+export AZURE_CLIENT_ID="5a50ed53-0a78-4b7b-9b74-c301a663aed6"
+export AZURE_CLIENT_SECRET="mrD8Q~OkYGaKHpLugvoI0hrWuQiEXj8usDsj7atv"
+export VAULT_URL="https://key-edge-dev-infra-001.vault.azure.net"
+export AZURE_TENANT_ID="6bc44698-b191-4f2e-97ac-cd7b430088cf"
 
 
 #download python script
 #FILEPATH=""
-#mkdir certificates
+mkdir certificates
 #cd certificates
 #git clone https://github.com/VikashniM/hc.git
 #cd hc
